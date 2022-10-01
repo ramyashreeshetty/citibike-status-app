1e0ee31bacf9fd9f88cc961500fb677287addcb6
Ramyashree shetty <Ramyashetty18042000@gmail.com>
2022-10-02 00:23:51 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,7 +18,6 @@ id_list = df[0].values.tolist()
 option = streamlit.selectbox('Choose the station id to view the status:', list(id_list))
 if streamlit.button('show status'):
   my_cur.execute("select * from station_status where id= '" + option + "';")
-  res=my_cur.fetchone()
-  rs = pandas.DataFrame(res)
-  streamlit.write(rs)               
-
+  df2 = my_cur.fetchone()
+  streamlit.write(df2[3])
+          
