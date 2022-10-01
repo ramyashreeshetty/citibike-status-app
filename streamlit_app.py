d0ac94d452d42bc05701f0b73e08bed9a8300290
Ramyashree shetty <Ramyashetty18042000@gmail.com>
2022-10-01 23:49:10 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,3 +5,13 @@ import pandas
 
 streamlit.title('Citibike station')
 
+# connect to snowflake
+my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+my_cur = my_cnx.cursor()
+
+# run a snowflake query and put it all in a var called my_catalog
+my_cur.execute("select station to know the status")
+my_catalog = my_cur.fetchall()
+
+# put the dafta into a dataframe
+df = pandas.DataFrame(my_catalog)
