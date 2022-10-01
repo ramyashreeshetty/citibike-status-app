dedbd463d944d4a788a091e125c4b4025bb4f255
Ramyashree shetty <Ramyashetty18042000@gmail.com>
2022-10-02 02:30:56 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,15 +15,16 @@ my_catalog = my_cur.fetchall()
 df = pd.DataFrame(my_catalog,columns=['id','legacy_id','station_status','bikes_available','bikes_disabled','rented_bikes','returned_bikes','ebikes','last_reported'])
 df.columns = map(lambda x: str(x).upper(), df.columns)
 #streamlit.write(df.columns)
+streamlit.write(df)
 
-id_list = df['ID'].values.tolist()
-#streamlit.write(id_list)
+# id_list = df['ID'].values.tolist()
+# #streamlit.write(id_list)
 
-option = streamlit.selectbox('Choose the station id to view the status:', list(id_list))
-if streamlit.button('show status'):
-          my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-          my_cur = my_cnx.cursor()
-          my_cur.execute("select * from station_status where 'ID' = '" + option + "';")
-          df2 = my_cur.fetchall()
-          streamlit.write(df2)
+# option = streamlit.selectbox('Choose the station id to view the status:', list(id_list))
+# if streamlit.button('show status'):
+#           my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+#           my_cur = my_cnx.cursor()
+#           my_cur.execute("select * from station_status where 'ID' = '" + option + "';")
+#           df2 = my_cur.fetchall()
+#           streamlit.write(df2)
           
