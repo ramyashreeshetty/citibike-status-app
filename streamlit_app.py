b8fe744bdbada266c5c8ff81d2a12b68f105d27e
Ramyashree shetty <Ramyashetty18042000@gmail.com>
2022-10-02 00:07:38 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,12 +5,15 @@ from urllib.error import URLError
 
 streamlit.title('Citibike station')
 
+# connect to snowflake
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 
-my_cur.execute("select * from station_status")
-my_data_row = my_cur.fetchone()
-streamlit.text("Hello from Snowflake:")
-streamlit.text(my_data_row)
+my_cur.execute("select * from station_status)
+my_catalog = my_cur.fetchall()
+
+df = pandas.DataFrame(my_catalog)
+streamlit.write(df)
+
 
 
