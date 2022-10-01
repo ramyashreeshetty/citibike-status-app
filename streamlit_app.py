2520364e7e88f1d15a71f7ba78fb3123c7b5c1bd
Ramyashree shetty <Ramyashetty18042000@gmail.com>
2022-10-02 02:07:53 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,11 +13,11 @@ my_cur.execute("select * from station_status")
 my_catalog = my_cur.fetchall()
 
 df = pd.DataFrame(my_catalog,columns=['id','legacy_id','station_status','bikes_available','bikes_disabled','rented_bikes','returned_bikes','ebikes','last_reported'])
-df.columns = map(lambda x: str(x).upper(), df.columns)
+#df.columns = map(lambda x: str(x).upper(), df.columns)
 streamlit.write(df.columns)
+
 id_list = df[0].values.tolist()
-df.columns = df.columns.str.upper()
-#streamlit.write(id_list)
+streamlit.write(id_list)
 
 option = streamlit.selectbox('Choose the station id to view the status:', list(id_list))
 if streamlit.button('show status'):
