3974c10a2af79e520de54c0944555c24afc03e93
Ramyashree shetty <Ramyashetty18042000@gmail.com>
2022-10-02 00:05:56 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,7 +7,8 @@ streamlit.title('Citibike station')
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
-my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(),CURRENT_REGION()")
+
+my_cur.execute("select * from station_status")
 my_data_row = my_cur.fetchone()
 streamlit.text("Hello from Snowflake:")
 streamlit.text(my_data_row)
