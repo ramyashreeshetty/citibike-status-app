66a1c45ee08e455c15537616bab17596fd3fc717
Ramyashree shetty <Ramyashetty18042000@gmail.com>
2022-10-02 00:38:24 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,9 +18,10 @@ id_list = df[0].values.tolist()
 #streamlit.write(id_list)
 
 option = streamlit.selectbox('Choose the station id to view the status:', list(id_list))
-streamlit.write(option)
-# if streamlit.button('show status'):
-#           my_cur.execute("select id from station_status where id= '" + option + "' ")
-#           df2 = my_cur.fetchone()
-#           streamlit.write(df2[0])
+if streamlit.button('show status'):
+          my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+          my_cur = my_cnx.cursor()
+          my_cur.execute("select id from station_status where id= '" + option + "' ")
+          df2 = my_cur.fetchone()
+          streamlit.write(df2[0])
           
