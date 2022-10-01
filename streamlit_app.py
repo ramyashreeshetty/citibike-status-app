cf0279cb7240a24f91257a511027635dfcee50f2
Ramyashree shetty <Ramyashetty18042000@gmail.com>
2022-10-01 23:50:55 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,7 +11,6 @@ my_cur = my_cnx.cursor()
 
 # run a snowflake query and put it all in a var called my_catalog
 my_cur.execute("select station to know the status")
-my_catalog = my_cur.fetchall()
 
-# put the dafta into a dataframe
-df = pandas.DataFrame(my_catalog)
+
+
