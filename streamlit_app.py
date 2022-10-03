402d3c6f2f2fea45f19a0ab7fc0ff87d9039fc13
mohakgupt <100181077+mohakgupt@users.noreply.github.com>
2022-10-03 21:55:50 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,7 +27,9 @@ if streamlit.button('show status'):
           my_cur = my_cnx.cursor()
           my_cur.execute("""select * from citibike_status where "id" = """ + option + """; """)
           res = my_cur.fetchall()
-          df2=pd.DataFrame(res,columns=hdrs['name'])
-          streamlit.write(df2)
+          df2=pd.DataFrame(res,columns=hdrs['name']).loc[0]
+          for c in hdrs['name']:
+                    streamlit.write(df2.at[c])
+          
   
           
