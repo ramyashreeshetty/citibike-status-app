ada344d70e71e3e6752a1e20acb22c7c28ecaae3
Ujjwal Mahar <111011580+UjjwalMahar@users.noreply.github.com>
2022-10-04 02:53:10 -0700

    Update streamlit_app.py
    Made titile S capital and added shade + border in show status button

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ import pandas as pd
 import snowflake.connector
 from urllib.error import URLError
 
-streamlit.title('Citibike station')
+streamlit.title('Citibike Station')
 
 
 #Adding Background Image
@@ -13,6 +13,11 @@ streamlit.markdown(
     <style>
     [class="main css-k1vhr4 egzxvld3"] {
         background-color: lightblue;
+    }
+    
+    [class="css-1g1an1w edgvbvh9"]{
+    background-color: #EEEEEE:
+    border; 1px solid #DCDCDC;
     }
     </style>
     """,
