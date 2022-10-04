1c42d1091e1e910a4b7b52f0327b93f7aa7daf86
Ashish-Abraham <ashishabraham22@gmail.com>
2022-10-04 18:41:29 +0530

    adjust animation boundaries

--- streamlit_app.py
+++ streamlit_app.py
@@ -37,7 +37,7 @@ lottie_hello = load_lottieurl(lottie_url_hello)
 
 
 
-st_lottie(lottie_hello, key="hello", height=400, width=400, renderer= "canvas")
+st_lottie(lottie_hello, key="hello", height=400, width=400)
 
 
 option = streamlit.selectbox('Choose the station id to view the status:', list(id_list))
