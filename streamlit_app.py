71fe210356e8854260de786b7dbe0140d03defbe
Ramyashree shetty <Ramyashetty18042000@gmail.com>
2022-10-07 13:35:57 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -43,16 +43,16 @@ hdrs = pd.DataFrame(my_cur.description)
 id_list = df[0].values.tolist()
 #streamlit.write(id_list)
 
-def load_lottieurl(url: str):
-    r = requests.get(url)
-    if r.status_code != 200:
-        return None
-    return r.json()
+# def load_lottieurl(url: str):
+#     r = requests.get(url)
+#     if r.status_code != 200:
+#         return None
+#     return r.json()
 
 
-lottie_url_hello = "https://assets2.lottiefiles.com/packages/lf20_rxevbe2y.json"
-lottie_hello = load_lottieurl(lottie_url_hello)
-st_lottie(lottie_hello, key="hello")
+# lottie_url_hello = "https://assets2.lottiefiles.com/packages/lf20_rxevbe2y.json"
+# lottie_hello = load_lottieurl(lottie_url_hello)
+# st_lottie(lottie_hello, key="hello")
 
 
 option = streamlit.selectbox('Choose the station id to view the status:', list(id_list))
