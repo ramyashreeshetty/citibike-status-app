b7d59f55f1a9034ea8d76d292c16ec1ed216f2b1
Ujjwal Mahar <111011580+UjjwalMahar@users.noreply.github.com>
2022-10-08 10:28:16 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -29,11 +29,11 @@ if __name__ == "__main__":
         """
     <style>
     [class="main css-k1vhr4 egzxvld3"] {
-       background-color: #E5E5F7;
-       background-color: #e5e5f7;
-       background: linear-gradient(135deg, #444cf755 25%, transparent 25%) -10px 0/ 20px 20px, linear-gradient(225deg, #444cf7 25%, transparent 25%) -10px 0/ 20px 20px, linear-gradient(315deg, #444cf755 25%, transparent 25%) 0px 0/ 20px 20px, linear-gradient(45deg, #444cf7 25%, #e5e5f7 25%) 0px 0/ 20px 20px;
-       }
-    
+          background-color: #E5E5F7;
+          background-color: #89e1e2;
+          opacity: 0.3;
+          background: linear-gradient(135deg, #444cf755 25%, transparent 25%) -20px 0/ 40px 40px, linear-gradient(225deg, #444cf7 25%, transparent 25%) -20px 0/ 40px 40px, linear-gradient(315deg, #444cf755 25%, transparent 25%) 0px 0/ 40px 40px, linear-gradient(45deg, #444cf7 25%, #89e1e2 25%) 0px 0/ 40px 40px;
+    }
     [class="css-1g1an1w edgvbvh9"]{
     background-color: #EEEEEE;
     border: 2px solid #DCDCDC;
